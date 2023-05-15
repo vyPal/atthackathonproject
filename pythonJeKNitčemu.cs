19010fc64955f72443464ce0b698c033b3eef707
Spjata <vojtavitcz@seznam.cz>
2023-05-15 15:56:56 +0200

    ja bych ty pythony zakazal

---
+++ pythonJeKNitčemu.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace python
+{
+    class python
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Python je k nitčemu!");
+            Console.WriteLine("Bych ty pythony zakázal!");
+        }
+    }
+}
