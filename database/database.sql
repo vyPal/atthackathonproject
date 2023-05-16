2d076a7bf5633f97e5cef774921358e1aa8318a3
Spjata <vojtavitcz@seznam.cz>
2023-05-16 12:23:04 +0200

    patient table

--- database/database.sql
+++ database/database.sql
@@ -6,7 +6,18 @@ GO
 IF NOT EXISTS (
     SELECT name
         FROM sys.databases
-        WHERE name = N'DatabaseName'
+        WHERE name = N'ourdatabase'
 )
-CREATE DATABASE database
+CREATE DATABASE ourdatabase
 GO
+
+-- Tohle uz jsem psal ja (SPRATEK) to nad tim mi napsalo rozsireni
+CREATE TABLE patients
+(
+    secondName VARCHAR(10),
+    firstName VARCHAR(10),
+    birthDate DATA,
+    insuredNumber INT(10) PRIMARY KEY,
+    zpCode int(3),
+)
+
