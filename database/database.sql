cea34bfb8ebbbfdd15657601d42f9492cfb47d9b
vyPal <kubik.palacky@gmail.com>
2023-05-16 13:06:46 +0200

    Idk
    Co-authored-by: JRK <JRKdotEXE@users.noreply.github.com>
    Co-authored-by: Rustatano <Rustatano@users.noreply.github.com>
    Co-authored-by: RybakVonTar <RybakVonTar@users.noreply.github.com>
    Co-authored-by: Bastien000 <Bastien000@users.noreply.github.com>

--- database/database.sql
+++ database/database.sql
@@ -12,6 +12,8 @@ CREATE DATABASE ourdatabase
 GO
 
 -- Tohle uz jsem psal ja (SPRATEK) to nad tim mi napsalo rozsireni
+
+-- lide tables
 CREATE TABLE patients
 (
     uuid VARCHAR(11),
@@ -23,18 +25,36 @@ CREATE TABLE patients
     zpCode INT(3),
 )
 
+CREATE TABLE doctors
+(
+    userName VARCHAR(10),
+    pasword VARCHAR(10),
+    secondName VARCHAR(20),
+    firstName VARCHAR(20),
+    phoneNumber INT(9),
+)
+
 -- recipe tables
 CREATE TABLE recipe
 (
     recipeID INT(4) PRIMARY KEY,
     patientInsuredNumber INT(10) PRIMARY KEY,
-    medicationType VARCHAR(50),
+    medicationType VARCHAR(50), -- bude z tabulky medication
+    doctor VARCHAR(40), -- bude z tabulky doctors
 )
 
 CREATE TABLE medication
 (
     medicationName VARCHAR(50) PRIMARY KEY,
-    dosage VARCHAR(5), -- RANO-POLEDNE-VECER
+    dosage VARCHAR(5) PRIMARY KEY, -- RANO-POLEDNE-VECER
     amount INT(2),
 )
 -- end of recipe tables
+
+CREATE TABLE medicalReport
+(
+    medicalReportID INT(4) PRIMARY KEY,
+    patientInsuredNumber INT(10) PRIMARY KEY,
+    reportDate DATA,
+    diagnose VARCHAR(2000),
+)
