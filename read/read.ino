087e35d880a428d28122d64297b0c238dea7355a
Rustatano <jakub-svoboda111@seznam.cz>
2023-05-18 16:51:35 +0200

    posilani UID pres Bluetooth

--- read/read.ino
+++ read/read.ino
@@ -1,49 +1,35 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include "BluetoothSerial.h"
  
 #define SS_PIN 5
 #define RST_PIN 27
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
-  // pinMode(, OUTPUT);
-  // pinMode(, OUTPUT);
-  // pinMode(, OUTPUT);
-  Serial.println("reading...");  // sviceni zlute LEDky misto print
-  // digitalWrite(, HIGH)  // set 
+  Serial.println("reading...");
   Serial.println();
+  SerialBT.begin("animal");
 }
 
-void loop() {
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+void loop() {  
+
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial()) {  // check if chip is present
     return;
   }
 
-  String content = "";  // get UID
-  byte letter;
+  String id = "";  // get UID
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+     id.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     id.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  content.toUpperCase();
-  
-  Serial.println();
-  Serial.println("Rozsvitit zelenou LEDku");  // rozsviceni zelene LEDky misto print
-  Serial.println("poslat UID do raspberry");
-  delay(3000);
-  /*
-  digitalWrite(, LOW);  // set yellow to LOW
-  digitalWrite(, HIGH);  // set green to HIGH
-  Serial.println("poslat UID do raspberry");
-  delay(3000);
-  // pokud prijde signal z raspberry, ze UID neni v databazi -> rozsvitit cervenou LEDku
-  */
-} 
+  id.toUpperCase();
+
+  SerialBT.println(id);  // send UID
+
+  delay(2500);
+}
